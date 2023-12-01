dac836fb8e6ae87917468f37b4e4fd8ded939ca1
brrm <bruce.mauger@gmail.com>
2023-12-01 08:16:54 +0000

    fix sklearn bug

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ VERSION = "0.1.0"
 REQUIRED = [
     "pandas", "numpy", "jupyter", "matplotlib",
     "pymysql", "geopandas", "statsmodels", "osmnx",  
-    "seaborn", "sklearn"
+    "seaborn", "scikit-learn"
 ]
 
 # What packages are optional?
